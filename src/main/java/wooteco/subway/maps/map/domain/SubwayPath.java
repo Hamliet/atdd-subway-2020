5fbc06a0b2d039879214b4b4c0e1ab183eb98ef8
hamliet <hamliet@hanyang.ac.kr>
2020-08-06 19:02:30 +0900

    refactor: 연령별 할인 기능 코드 리팩토링

--- src/main/java/wooteco/subway/maps/map/domain/SubwayPath.java
+++ src/main/java/wooteco/subway/maps/map/domain/SubwayPath.java
@@ -8,6 +8,17 @@ import wooteco.subway.members.member.domain.LoginMember;
 
 public class SubwayPath {
 
+    private static final int DEFAULT_DISTANCE_FARE = 1250;
+    private static final int DEFAULT_DISTANCE = 10;
+    private static final int CHILDREN_MIN_AGE = 6;
+    private static final int CHILDREN_MAX_AGE = 12;
+    private static final int YOUTH_MIN_AGE = 13;
+    private static final int YOUTH_MAX_AGE = 18;
+    private static final int YOUNG_DEFAULT_DISCOUNT = 350;
+    private static final double YOUTH_DISCOUNT_RATE = 0.2;
+    private static final double CHILDREN_DISCOUNT_RATE = 0.5;
+    private static final int OVER_FARE_UNIT = 100;
+
     private List<LineStationEdge> lineStationEdges;
 
     public SubwayPath(List<LineStationEdge> lineStationEdges) {
@@ -37,17 +48,16 @@ public class SubwayPath {
     }
 
     public int calculateTotalFare() {
-        int totalDistance = calculateDistance();
-        int defaultDistanceFare = 1250;
-        int overDistanceFare = calculateOverFare(totalDistance - 10);
+        int overDistanceFare = calculateOverFare(calculateDistance() - DEFAULT_DISTANCE);
         int maximumExtraFare = lineStationEdges.stream()
             .mapToInt(it -> it.getLineStation().getExtraFare()).max()
             .orElseThrow(() -> new IllegalArgumentException("extraFare가 설정되지 않았습니다."));
+
         LoginMember loginMember = (LoginMember) SecurityContextHolder.getContext()
             .getAuthentication().getPrincipal();
-        int notDiscountedFare = (defaultDistanceFare + overDistanceFare + maximumExtraFare);
+        int notDiscountedFare = (DEFAULT_DISTANCE_FARE + overDistanceFare + maximumExtraFare);
 
-        return (int) calculateDiscountedFare(notDiscountedFare, loginMember.getAge());
+        return calculateDiscountedFare(notDiscountedFare, loginMember.getAge());
     }
 
     private int calculateOverFare(int distance) {
@@ -55,17 +65,18 @@ public class SubwayPath {
             return 0;
         }
         if (distance <= 40) {
-            return (int) ((Math.ceil((distance - 1) / 5) + 1) * 100);
+            return (int) ((Math.ceil((distance - 1) / 5) + 1) * OVER_FARE_UNIT);
         }
-        return (int) ((Math.ceil((distance - 40 - 1) / 8) + 1) * 100) + calculateOverFare(40);
+        return (int) ((Math.ceil((distance - 40 - 1) / 8) + 1) * OVER_FARE_UNIT)
+            + calculateOverFare(40);
     }
 
-    private double calculateDiscountedFare(Integer notDiscountedFare, Integer age) {
-        if (6 <= age && age < 13) {
-            return (notDiscountedFare - 350) * 0.5;
+    private int calculateDiscountedFare(int notDiscountedFare, int age) {
+        if (CHILDREN_MIN_AGE <= age && age <= CHILDREN_MAX_AGE) {
+            return (int) ((notDiscountedFare - YOUNG_DEFAULT_DISCOUNT) * CHILDREN_DISCOUNT_RATE);
         }
-        if (13 <= age && age < 19) {
-            return (notDiscountedFare - 350) * 0.2;
+        if (YOUTH_MIN_AGE <= age && age <= YOUTH_MAX_AGE) {
+            return (int) ((notDiscountedFare - YOUNG_DEFAULT_DISCOUNT) * YOUTH_DISCOUNT_RATE);
         }
         return 1;
     }
