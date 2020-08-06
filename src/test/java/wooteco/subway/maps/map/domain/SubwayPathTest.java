7abda29fecf4a347ef52e0bd513d037481953ea4
hamliet <hamliet@hanyang.ac.kr>
2020-08-06 18:37:42 +0900

    feat: 연령별 할인 테스트 구현

--- src/test/java/wooteco/subway/maps/map/domain/SubwayPathTest.java
+++ src/test/java/wooteco/subway/maps/map/domain/SubwayPathTest.java
@@ -1,23 +1,52 @@
 package wooteco.subway.maps.map.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static wooteco.subway.members.member.acceptance.step.MemberAcceptanceStep.내_회원_정보_조회_요청;
-import static wooteco.subway.members.member.acceptance.step.MemberAcceptanceStep.로그인_되어_있음;
-import static wooteco.subway.members.member.acceptance.step.MemberAcceptanceStep.회원_등록되어_있음;
 
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Test;
-import wooteco.security.core.TokenResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import wooteco.security.core.Authentication;
+import wooteco.security.core.context.SecurityContextHolder;
+import wooteco.security.core.userdetails.UserDetails;
 import wooteco.subway.maps.line.domain.LineStation;
+import wooteco.subway.members.member.application.CustomUserDetailsService;
+import wooteco.subway.members.member.application.MemberService;
+import wooteco.subway.members.member.domain.Member;
+import wooteco.subway.members.member.domain.MemberRepository;
+import wooteco.subway.members.member.dto.MemberRequest;
+import wooteco.subway.members.member.dto.MemberResponse;
 
+@SpringBootTest
 class SubwayPathTest {
 
     private static final String EMAIL = "email@email.com";
     private static final String PASSWORD = "password";
     private static final Integer AGE = 20;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    MemberService memberService;
+
+    private Member createMember(String email, String nickname, int age) {
+        MemberRequest memberRequest = new MemberRequest(email, nickname, age);
+        MemberResponse memberResponse = memberService.createMember(memberRequest);
+
+        return memberRepository.findById(memberResponse.getId())
+            .orElseThrow(() -> new IllegalArgumentException("해당하는 사용자가 존재하지 않습니다."));
+    }
+
+    private void setAuthentication(Member member) {
+        CustomUserDetailsService customUserDetailsService = new CustomUserDetailsService(
+            memberRepository);
+        UserDetails userDetails = customUserDetailsService
+            .loadUserByUsername(member.getEmail());
+        Authentication authentication = new Authentication(userDetails);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
 
     @Test
     void calculateTotalFare() {
@@ -27,8 +56,10 @@ class SubwayPathTest {
             new LineStationEdge(new LineStation(3L, 2L, 15, 1, 500), 1L)
         );
         SubwayPath subwayPath = new SubwayPath(lineStationEdges);
+        Member member = createMember(EMAIL, PASSWORD, 10);
+        setAuthentication(member);
 
-        // 성인, 노선 추가 요금 500, 이용 거리 15km 추가
-        assertThat(subwayPath.calculateTotalFare()).isEqualTo(2050);
+        // 어린이, 노선 추가 요금 500, 이용 거리 15km 추가
+        assertThat(subwayPath.calculateTotalFare()).isEqualTo(1025);
     }
 }
